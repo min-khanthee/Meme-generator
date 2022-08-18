18996058a06ca88e54ad48e46cca63b2823e8896
Min Khanthee <pkhanthee@gmail.com>
2022-08-18 12:19:45 +1200

    renderender memes 🖼

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,22 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import memesData from '../memesData.js'
 
 export default function Meme() {
-  /**
-   * Challenge: Save the random meme URL in state
-   * - Create new state called `memeImage` with an
-   *   empty string as default
-   * - When the getMemeImage function is called, update
-   *   the `memeImage` state to be the random chosen
-   *   image URL
-   * - Below the div.form, add an <img /> and set the
-   *   src to the new `memeImage` state you created
-   */
+  const [memeImage, setMemeImage] = useState('')
 
   function getMemeImage() {
-    // const memesArray = memesData.data.memes
-    // const randomNumber = Math.floor(Math.random() * memesArray.length)
-    // memesArray[randomNumber].url  <-- this line is incomplete!
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+
+    setMemeImage(memesArray[randomNumber].url)
   }
 
   return (
@@ -28,6 +20,7 @@ export default function Meme() {
           Get a new meme image 🖼
         </button>
       </div>
+      <img src={memeImage} className='meme--image' />
     </main>
   )
 }
